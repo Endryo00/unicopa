e365bbd48f8f81e8edd0af1c85ea1064f91505a4
Endryo <endryo220501@edu.unipar.br>
2026-05-18 20:15:46 -0300

    junção com o repo do Ryan, criação do RF6 e 7.

---
+++ App.js
@@ -0,0 +1,152 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  SectionList,
+} from "react-native";
+import GameCard from "./app/components/GameCard";
+import DiaCard from "./app/components/DiaCard";
+import copaData from "./app/assets/data/copaData.json";
+import { agruparPorData } from "./app/utils/jogoUtils";
+import { useState } from "react";
+
+export default function App() {
+  const [jogos, setJogos] = useState(copaData.jogos);
+  const [dadosCopa, setDadosCopa] = useState(copaData);
+  const [favoritos, setFavoritos] = useState([]);
+
+  const hoje = new Date().toISOString().split("T")[0];
+
+  const jogosAgrupados = agruparPorData(jogos);
+  const jogosTratados = Object.keys(jogosAgrupados).map((data) => {
+    const jogosOrdenados = jogosAgrupados[data].sort((a, b) => {
+      return a.hora_brasilia.localeCompare(b.hora_brasilia);
+    });
+    return {
+      title: data,
+      data: jogosAgrupados[data],
+    };
+  });
+
+  function handleToggleFavorito(jogoId) {
+    setFavoritos((prev) =>
+      prev.includes(jogoId)
+        ? prev.filter((id) => id !== jogoId)
+        : [...prev, jogoId],
+    );
+  }
+
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require("./app/assets/bg-overlay.png")}
+    >
+      <Image style={styles.logo} source={require("./app/assets/unicopa.png")} />
+
+      <Text style={styles.title}>CALENDÁRIO</Text>
+      <SectionList
+        sections={jogosTratados}
+        keyExtractor={(item, index) => item.id || index.toString()}
+        renderItem={() => null}
+        renderSectionHeader={({ section }) => {
+          const ehHoje = section.title === hoje;
+
+          return (
+            <DiaCard
+              data={section.title}
+              jogos={section.data}
+              favoritos={favoritos}
+              onToggleFavorito={handleToggleFavorito}
+              isHoje={ehHoje}
+            />
+          );
+        }}
+      />
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%",
+    backgroundColor: "#040b13",
+    alignItems: "center",
+  },
+  logo: {
+    marginTop: 20,
+    width: 200,
+    height: 50,
+    resizeMode: "contain",
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 28,
+    fontWeight: "700",
+    color: "white",
+  },
+  card: {
+    marginTop: 20,
+    backgroundColor: "#0c1b2a",
+    width: 320,
+    borderRadius: 12,
+    padding: 15,
+  },
+  data: {
+    color: "#f2cc2f",
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+
+  jogo: {
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#1e2d3d",
+    paddingBottom: 15,
+  },
+  grupo: {
+    color: "#8fa3b8",
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  linhaPrincipal: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  time: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  bandeira: {
+    width: 28,
+    height: 28,
+    borderRadius: 14,
+  },
+  sigla: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  horario: {
+    alignItems: "center",
+  },
+  hora: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  local: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  subTitulo: {
+    color: "#8fa3b8",
+    fontSize: 12,
+  },
+});
---
+++ app/components/DiaCard.jsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from "react-native";
+import GameCard from "./GameCard";
+import { formatarDataDiaMes } from "../utils/jogoUtils";
+
+export default function DiaCard({ data, jogos, favoritos, onToggleFavorito, isHoje}) {
+  const dataFormatada = formatarDataDiaMes(data);
+
+  return (
+    <View style={styles.card, isHoje && styles.cardHoje}>
+      <Text style={[styles.data, isHoje && styles.textHoje]}>
+        {data} {isHoje && "(HOJE)"}
+      </Text>
+      <Text style={styles.data}>{dataFormatada}</Text>
+      {jogos.map((jogo) => (
+        <GameCard
+          key={jogo.id}
+          game={jogo}
+          isFavorito={favoritos.includes(jogo.id)}
+          onToggleFavorito={onToggleFavorito}
+        />
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginTop: 20,
+    backgroundColor: "#0c1b2a",
+    width: 320,
+    borderRadius: 12,
+    padding: 15,
+  },
+  data: {
+    color: "#f2cc2f",
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  cardHoje: {
+    borderColor: "#f2cc2f", // Uma borda amarela/ouro para destacar
+    borderWidth: 2,
+  },
+  textHoje: {
+    color: "#00ff7f", // Deixa o texto verde brilhante ou outra cor de destaque
+  }
+});
